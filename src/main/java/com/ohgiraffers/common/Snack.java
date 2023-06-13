d0ea1a78cb2a1a9daaba6c7cc9d69373e9ebc867
bbbbooo <105257665+bbbbooo@users.noreply.github.com>
2023-06-13 16:52:38 +0900

    snack, snackdao 추가

--- src/main/java/com/ohgiraffers/common/Snack.java
+++ src/main/java/com/ohgiraffers/common/Snack.java
@@ -1,4 +1,14 @@
 package com.ohgiraffers.common;
 
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
 public class Snack {
+    private int number;
+    private String snackName;
+    private int price;
 }
--- src/main/java/com/ohgiraffers/common/SnackDAO.java
+++ src/main/java/com/ohgiraffers/common/SnackDAO.java
@@ -1,4 +1,21 @@
 package com.ohgiraffers.common;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+
+@Repository
 public class SnackDAO {
+    private final HashMap<Integer, Snack> snackMap;
+
+    public SnackDAO(){
+        this.snackMap = new HashMap<>();
+
+        this.snackMap.put(1, new Snack(1, "초코", 2000));
+        this.snackMap.put(2, new Snack(2, "바닐라", 2000));
+        this.snackMap.put(3, new Snack(3, "딸기", 2000));
+        this.snackMap.put(4, new Snack(4, "사브레", 2000));
+    }
+
+
 }
